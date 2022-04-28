0320c41e02aae77e687e73ce125ab4dffa9a8f5d
mihostet <michi.hostettler@cern.ch>
2022-04-28 10:27:56 +0200

    fix and add tests

--- stubgenj/tests/test_stubtest.py
+++ stubgenj/tests/test_stubtest.py
@@ -48,4 +48,4 @@ def test_generate_stubs(stub_tmpdir):
 
 @pytest.mark.trylast
 class StubTestSuite(mypy.test.testcheck.TypeCheckSuite):
-    files = ['arraylist.test', 'hashmap.test', 'enummap.test', 'callbacks.test', 'jpype_jpackage.test']
+    files = ['arraylist.test', 'hashmap.test', 'enummap.test', 'callbacks.test', 'jpype_jpackage.test', 'varargs.test']
