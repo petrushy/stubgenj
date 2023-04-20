c98e6b155d96dafea8c9da7693a2e4d098cf3ed9
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2023-04-20 10:12:15 +0200

    Add jpype.JException supertype for java.lang.Throwable

--- stubgenj/_stubgenj.py
+++ stubgenj/_stubgenj.py
@@ -1111,6 +1111,10 @@ def jpypeCustomizerSuperTypes(jClass: jpype.JClass, classTypeVars: List[TypeVarS
             typeStr += '[' + ', '.join([tv.pythonName for tv in classTypeVars]) + ']'
         extraSuperTypes.append(typeStr)
         customizersUsed.add(customizer)
+    if jClass.class_.getName() == 'java.lang.Throwable':
+        # Workaround to allow Throwable-derived exception types be recognized
+        # as JException, so that they can be assigned as Exception.__cause__
+        extraSuperTypes.append('jpype.JException')
     return extraSuperTypes
 
 
